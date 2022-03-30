aa28d57dd54806c53b259d1b1fb6018cf63f95b6
hedil <chedil@yahoo.fr>
2022-03-30 19:16:32 +0200

    première version

---
+++ src/main/java/com/openclassrooms/safetynetalertsP5/SafetynetalertsP5Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynetalertsP5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertsP5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertsP5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/safetynetalertsP5/SafetynetalertsP5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynetalertsP5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetalertsP5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
